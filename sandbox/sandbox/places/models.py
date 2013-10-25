ed353f3ee90af1d1b12e79ad0cdc5eae5dd86745
Andrew Ingram <andy@andrewingram.net>
2013-10-25 14:31:53 +0100

    rough attempt at an example of distance querying for mysql

--- sandbox/sandbox/places/models.py
+++ sandbox/sandbox/places/models.py
@@ -1,5 +1,5 @@
 from django.contrib.gis.db import models
-
+from transit.models import Station
 
 class Place(models.Model):
     name = models.CharField(max_length=255)
@@ -8,5 +8,18 @@ class Place(models.Model):
     objects = models.GeoManager()
 
     def nearest_stations(self):
-        from transit.models import Station
         return Station.objects.distance(self.position).prefetch_related('routes').order_by('distance')[:5]
+
+    def nearest_stations_mysql(self):
+        return Station.objects.prefetch_related('routes').extra(
+            select={
+            'distance': '''(6371 * acos(
+                            cos(radians(%s))
+                            * cos(radians(Y(position)))
+                            * cos(radians(X(position)) - radians(%s))
+                            + sin(radians(%s))
+                            * sin(radians(Y(position)))
+                            ))'''
+            },
+            select_params=(self.position.y, self.position.x, self.position.y)
+        ).order_by('distance')[:5]
