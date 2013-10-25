3b8cbe89c30d5c86a60a15a6ca0e7ad1055b3144
Andrew Ingram <andy@andrewingram.net>
2013-10-25 15:09:05 +0100

    config for london overground

--- transit/conf.py
+++ transit/conf.py
@@ -6,5 +6,6 @@ DATA_FILES = getattr(settings, 'TRANSIT_DATA_FILES', (
     'FR/PAR/rer.json',
     'FR/PAR/tramway.json',
     'GB/LON/tube.json',
+    'GB/LON/overground.json',
     'US/NYC/subway.json',
 ))
